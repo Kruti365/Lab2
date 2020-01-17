6a36750f2113a6219c9d7092113cfa005d8d8940
Kruti365 <55569084+Kruti365@users.noreply.github.com>
2020-01-17 12:48:42 -0500

    Double sort update

--- Kruti/Kruti3.js
+++ Kruti/Kruti3.js
@@ -1,7 +1,10 @@
-// https://www.codewars.com/kata/57cc79ec484cf991c900018d/train/javascript
+//https://www.codewars.com/kata/57cc79ec484cf991c900018d/train/javascript
 function dbSort(a){
 // Code here
 
-return a.sort();
+let arrayNumber=a.filter(x=> typeof x=="number");
+let arrayStrings=a.filter (y=>typeof y=="string");
 
-}
+return arrayNumber.sort((b,c)=>b-c).concat(arrayStrings.sort());
+
+ }
